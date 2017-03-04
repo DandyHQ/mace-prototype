ab855757ce52aff000badac408c2e2c47ce979e0
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-03-04 19:48:34 +1300

    header body examply frame

--- src/View.elm
+++ src/View.elm
@@ -1,9 +1,16 @@
 module View exposing (view)
 
-import Html exposing (Html, text)
+import Html exposing (..)
+import Html.Attributes exposing (style)
 import Model exposing (Model)
 import Msg exposing (Msg)
 
+(=>) =
+  (,)
+
 view : Model -> Html Msg
 view model =
-  text "Hello, World"
+  div [ style [ "display" => "flex", "flex-flow" => "column", "height" => "100%" ] ]
+    [ div [ style [ "flex" => "0 1 auto" ] ] [ text "header" ]
+    , textarea [ style [ "flex" => "1 1 auto" ] ] []
+    ]
