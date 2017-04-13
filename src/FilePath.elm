6d525c464f0a6bd0beb1dc8d0861aa945d1900c7
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-04-13 14:35:28 +1200

    correct behaviour

--- src/FilePath.elm
+++ src/FilePath.elm
@@ -8,15 +8,27 @@ takeFileName path =
 
 takeDirectory : String -> String
 takeDirectory path =
-  String.join "/" <| List.reverse <| List.drop 1 <| List.reverse <| String.split "/" path
+  let leading = if String.left 1 path == "/" then "/" else "" in
+  String.split "/" path
+    |> List.reverse
+    |> List.drop 1
+    |> List.reverse
+    |> List.filter ((/=) "")
+    |> String.join "/"
+    |> (++) leading
 
 takeExtension : String -> String
 takeExtension path =
-  Maybe.withDefault "" <| List.head <| List.reverse <| String.split "." path
+  takeFileName path
+    |> String.split "."
+    |> List.tail
+    |> Maybe.withDefault []
+    |> String.join "."
 
 dropExtension : String -> String
 dropExtension path =
-  String.join "." <| List.reverse <| List.drop 1 <| List.reverse <| String.split "." path
+  let extLength = String.length (takeExtension path) in
+  String.dropRight (extLength + if extLength /= 0 then 1 else 0) path
 
 takeBaseName : String -> String
 takeBaseName path =
